0efbb6ca3e8054ad2199c0cc378f5e2baac7c7a3
Ксения <koshelksenia050607@gmail.com>
2026-02-24 11:22:17 +0300

    Refactor: replace boilerplate code with Lombok

--- src/main/java/com/example/libraryapp/api/dto/BookResponseDto.java
+++ src/main/java/com/example/libraryapp/api/dto/BookResponseDto.java
@@ -1,40 +1,13 @@
 package com.example.libraryapp.api.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class BookResponseDto {
     private Long id;
     private String title;
     private String author;
     private Integer publicationYear;
-
-    public BookResponseDto() {
-        // Пустой конструктор нужен для создания объектов Jackson библиотекой
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Integer getPublicationYear() {
-        return publicationYear;
-    }
-    public void setPublicationYear(Integer publicationYear) {
-        this.publicationYear = publicationYear;
-    }
 }
--- src/main/java/com/example/libraryapp/domain/Book.java
+++ src/main/java/com/example/libraryapp/domain/Book.java
@@ -1,5 +1,12 @@
 package com.example.libraryapp.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     private Long id;
     private String isbn;
@@ -7,57 +14,4 @@ public class Book {
     private String author;
     private Integer publicationYear;
     private String genre;
-
-    public Book(Long id, String isbn, String title, String author,
-                Integer publicationYear, String genre) {
-        this.id = id;
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
-        this.genre = genre;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Integer getPublicationYear() {
-        return publicationYear;
-    }
-    public void setPublicationYear(Integer publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
 }
-
